039763639961645662f03a180c465d32ee37c1ab
KuniDevLua <YUUHAYATE2@gmail.com>
2025-07-20 21:40:33 +0800

    Update Full_Gui.lua

--- Full_Gui.lua
+++ Full_Gui.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGarden/refs/heads/main/Obfuscated-FULL-GUI"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Xdlrc4vyFg/0", true))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/VO2NPoUi56/0", true))()
